5f5f7e1b68bf2ca3261d97830af588b87ec2a315
HuuBang11 <huubang0312@gmail.com>
2025-08-23 17:57:33 +0700

    fix footer

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -53,7 +53,7 @@ function heroSwiper() {
       speed: 1500,
       loop: true,
       autoplay: {
-        delay: 3000,
+        delay: 3000
       },
       pagination: {
         el: el.querySelector(".swiper-pagination"),
@@ -63,7 +63,7 @@ function heroSwiper() {
             <button class="${className}">
               <span class="progress-bar-swiper"></span>
             </button>`;
-        },
+        }
       },
 
       on: {
@@ -104,8 +104,8 @@ function heroSwiper() {
               slideInner.style.transition = `${speed}ms ${easing}`;
             }
           });
-        },
-      },
+        }
+      }
     });
   });
 }
@@ -127,7 +127,7 @@ function sectionDesign() {
 
   $(".section-design .slider").css({
     left: +actPosition.left,
-    width: actWidth,
+    width: actWidth
   });
 
   if (!document.querySelector(".swiper-design-parallax")) return;
@@ -145,23 +145,23 @@ function sectionDesign() {
       loop: true,
       loopedSlides: 2,
       autoplay: {
-        delay: 2000,
+        delay: 2000
       },
       pagination: {
         el: el.querySelector(".swiper-pagination"),
-        type: "progressbar",
+        type: "progressbar"
       },
       navigation: {
         nextEl: el.querySelector(".swiper-button-next"),
-        prevEl: el.querySelector(".swiper-button-prev"),
+        prevEl: el.querySelector(".swiper-button-prev")
       },
       breakpoints: {
         991: {
           slidesPerView: 1.5,
           spaceBetween: 40,
-          autoplay: false,
-        },
-      },
+          autoplay: false
+        }
+      }
     });
     swipers.set(el, swiper); // Lưu instance Swiper
     return swiper;
@@ -203,7 +203,7 @@ function animateTextKaraoke() {
     const splitKaraoke = new SplitText(karaoke, {
       type: "words, chars",
       wordsClass: "word",
-      charsClass: "char",
+      charsClass: "char"
     });
 
     gsap.to(splitKaraoke.chars, {
@@ -216,8 +216,8 @@ function animateTextKaraoke() {
         start: "top 85%",
         end: "top 30%",
         // markers: true,
-        scrub: true,
-      },
+        scrub: true
+      }
     });
   });
 }
@@ -233,8 +233,14 @@ function svgSokoda() {
         el.classList.add("active-svg");
       },
 
-      once: true,
+      once: true
     });
+  });
+}
+
+function footer() {
+  $("footer .dropdown-info").on("click", function () {
+    $(this).toggleClass("active");
   });
 }
 const init = () => {
@@ -243,6 +249,7 @@ const init = () => {
   animateTextKaraoke();
   svgSokoda();
   sectionDesign();
+  footer();
 };
 preloadImages("img").then(() => {
   // Once images are preloaded, remove the 'loading' indicator/class from the body

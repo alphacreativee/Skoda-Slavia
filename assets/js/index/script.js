2d17a5969bc0020e7737192162aa07707bf9448e
Gary <hai.alphacreative@gmail.com>
2025-08-25 10:58:52 +0700

    optimize img car

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -258,7 +258,7 @@ function model3D() {
     sensitivity: 300,
     auto: true,
     drag: false,
-    imageArray: generateCarImages("assets/images/white", 25),
+    imageArray: generateCarImages("assets/images/white", 24),
   });
 }
 const init = () => {
